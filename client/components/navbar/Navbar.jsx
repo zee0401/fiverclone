6ffda68c856d6316112fe15e27ac16bd529b7bb0
zee0401 <41227625+zee0401@users.noreply.github.com>
2023-08-05 18:05:51 +0530

    Navbar complete

---
+++ client/components/navbar/Navbar.jsx
@@ -0,0 +1,85 @@
+// import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "john Doe",
+    isSeller: true,
+  };
+
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="text">fiverr</span>
+          <span className="dot">.</span>
+          {/* </Link> */}
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>SIgn in</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={handleClick}>
+              <img
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add new gigs</span>
+                      <span>Gigs</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>test</span>
+            <span>test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
