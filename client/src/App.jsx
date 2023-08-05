7929f57c9baa7bdf74128aae7347e1a72e200b6f
zee0401 <41227625+zee0401@users.noreply.github.com>
2023-08-05 18:47:37 +0530

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import Navbar from "../components/navbar/Navbar";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 };
