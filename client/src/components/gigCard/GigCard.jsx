0d6d567a32ac463be389e9eb580186f2e7db2d83
zee0401 <41227625+zee0401@users.noreply.github.com>
2023-08-08 15:44:50 +0530

    front end complete

---
+++ client/src/components/gigCard/GigCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./gigcard.scss";
+
+const GigCard = ({ item }) => {
+  return (
+    <Link to="/gig/123">
+      <div className="gigcard">
+        <img src={item.img} alt="" />
+        <div className="info">
+          <div className="user">
+            <img src={item.pp} alt="" />
+            <span>{item.username}</span>
+          </div>
+          <p>{item.desc}</p>
+          <div className="star">
+            <img src="./img/star.png" alt="" />
+            <span>{item.star}</span>
+          </div>
+        </div>
+        <div className="details">
+          <div className="details">
+            <img src="./imgheart.png" alt="" />
+            <span>Starting at</span>
+            <h2>
+              $ {item.price}
+              <sup>99</sup>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default GigCard;
--- client/src/pages/gigs/Gigs.jsx
+++ client/src/pages/gigs/Gigs.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import "./gigs.scss";
+import { gigs } from "../../data";
+import GigCard from "../../components/gigCard/GigCard";
 
 const Gigs = () => {
-  return <div>Gigs</div>;
+  const [open, setOpen] = useState(false);
+  const [sort, setSort] = useState("sales");
+
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  };
+
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">Liverr Graphics & Design </span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Liverr's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input ref={minRef} type="number" placeholder="min" />
+            <input ref={maxRef} type="number" placeholder="max" />
+            <button onClick={apply}>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">Sort by</span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+                <span onClick={() => reSort("sales")}>Popular</span>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="cards">
+          {gigs.map((gig) => (
+            <GigCard key={gig.id} item={gig} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gigs;
